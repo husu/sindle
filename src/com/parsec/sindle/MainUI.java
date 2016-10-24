9bb62db1eb7cff91bc613f99058e7776c28541b3
husu <azhong_husu@hotmail.com>
2016-10-24 22:29:07 +0800

    提交 1.15，调整了止损前最多赚的计算

--- src/com/parsec/sindle/MainUI.java
+++ src/com/parsec/sindle/MainUI.java
@@ -120,74 +120,79 @@ public class MainUI implements ActionListener {
                      new Thread(() -> {     //我觉得这么做不科学，但是。。。你想怎样
                          log.append("正在载入Excel表，么么哒" + "\n");
                          ExcelReader excelReader = new ExcelReader();
-
                          try {
-
                              int from = Integer.parseInt(this.maField.getText());
                              int to = Integer.parseInt(this.a60TextField.getText());
+                             int tableNum = to - from  +1  ;
+                             int pageSize = 60;
+                             int blockNum = tableNum / pageSize;
 
+                             blockNum =  tableNum%pageSize>0?blockNum+1:blockNum;
 
-                             if ((to - from + 1) > 9) {
-                                 log.append("正在复制并生成MA的中间数据，共有" + (to - from + 1) + "个表要生成，可想而知会很慢\n");
-                             } else {
-                                 log.append("正在复制并生成MA的中间数据，共有" + (to - from + 1) + "个表要生成\n");
-
+                             if(blockNum>1){
+                                 log.append("由于表的数量太多，将生成" + blockNum + "个文件\n");
                              }
-                             File maFile = excelReader.createMADataSheet(fc.getSelectedFile(), from, to);
+                             for(int n =0;n<blockNum;n++){
+                                 if(blockNum>1){
+                                     log.append("正在生成第"+(n+1)+"个文件\n");
+                                 }
+                                 int curFrom = from + pageSize*n;
+                                 int curTo = n==blockNum-1?to:(pageSize+curFrom-1);
+                                 if ((curTo - curFrom + 1) > 9) {
+                                     log.append("正在复制并生成MA的中间数据，共有" + (curTo - curFrom + 1) + "个表要生成，可想而知会很慢\n");
+                                 } else {
+                                     log.append("正在复制并生成MA的中间数据，共有" + (curTo - curFrom + 1) + "个表要生成\n");
+                                 }
+                                 File maFile = excelReader.createMADataSheet(fc.getSelectedFile(), curFrom, curTo);
+                                 log.append("生成MA中间数据完毕\n");
+                                 log.append("开始计算交易数据");
+
+                                 XlsData xlsData;
+                                 List<Map<String, String>> statList = new ArrayList<>();
+
+                                 Map<String, String> statMap = null;
+                                 for (int i = from; i <= to; i++) {
+
+                                     xlsData = excelReader.loadXls(maFile, i);
+
+                                     log.append("开始分析计算MA" + i + "数据，么么哒\n");
+
+                                     List<MarketData> tradeList = excelReader.analyseData(xlsData.getMdList(), xlsData.getStopLossLine(), i);
+
+                                     //总共交易次数
+                                     statMap = new HashMap<String, String>() {{
+                                         put("tradeCount", tradeList.size() + "");
+                                     }};
+
+                                     statMap.put("sumWithStop", String.format("Sum('MA%d汇总'!N2:N%d)",i,tradeList.size()+1));  //有止损汇总
+                                     statMap.put("sumWithoutStop",String.format("Sum('MA%d汇总'!$M$2:$M$%d)",i,tradeList.size()+1,xlsData.getStopLossLine())); //无止损汇总
+                                     statMap.put("winCountWithStop",String.format("COUNTIF('MA%d汇总'!$N$2:$N$%d,\">0\")",i,tradeList.size()+1)); //有止损盈利次数
+                                     statMap.put("lossCountWithStop",String.format("COUNTIF('MA%d汇总'!$N$2:$N$%d,\"<0\")",i,tradeList.size()+1)); //有止损亏损次数
+                                     statMap.put("maxWithStop",String.format("MAX('MA%d汇总'!$N$2:$N$%d)",i,tradeList.size()+1)); //单次最大盈利
+                                     statMap.put("minWithStop",String.format("MIN('MA%d汇总'!$N$2:$N$%d)",i,tradeList.size()+1));//单次最大亏损
+                                     statMap.put("winCountWithoutStop",String.format("COUNTIF('MA%d汇总'!$M$2:$M$%d,\">0\")",i,tradeList.size()+1));//无止损盈利次数
+                                     statMap.put("lossCountWithoutStop",String.format("COUNTIF('MA%d汇总'!$M$2:$M$%d,\"<0\")",i,tradeList.size()+1));//无止损亏损次数
+                                     statMap.put("maxWithoutStop",String.format("MAX('MA%d汇总'!$M$2:$M$%d)",i,tradeList.size()+1));//单次最大盈利
+                                     statMap.put("minWithoutStop",String.format("MIN('MA%d汇总'!$M$2:$M$%d)",i,tradeList.size()+1));//单次最大亏损
 
 
-                             log.append("生成MA中间数据完毕\n");
+                                     statList.add(statMap);//将统计的Map加入到统计的列表
+                                     excelReader.modify(maFile, tradeList, i);
 
 
-                             log.append("开始计算交易数据");
+                                     log.append("MA" + i + "处理完毕，亚克西\n");
+                                 }
 
 
-                             XlsData xlsData;
-                             List<Map<String, String>> statList = new ArrayList<>();
+                                 log.append("正在处理汇总表");
 
-                             Map<String, String> statMap = null;
+                                 excelReader.fullStatTable(maFile,statList,from);
 
+                                 log.append("处理汇总表结束\n");
 
-                             for (int i = from; i <= to; i++) {
-
-                                 xlsData = excelReader.loadXls(maFile, i);
-
-                                 log.append("开始分析计算MA" + i + "数据，么么哒\n");
-
-                                 List<MarketData> tradeList = excelReader.analyseData(xlsData.getMdList(), xlsData.getStopLossLine(), i);
-
-                                 //总共交易次数
-                                 statMap = new HashMap<String, String>() {{
-                                     put("tradeCount", tradeList.size() + "");
-                                 }};
-
-                                 statMap.put("sumWithStop", String.format("Sum('MA%d汇总'!N2:N%d)",i,tradeList.size()+1));  //有止损汇总
-                                 statMap.put("sumWithoutStop",String.format("Sum('MA%d汇总'!$M$2:$M$%d)",i,tradeList.size()+1,xlsData.getStopLossLine())); //无止损汇总
-                                 statMap.put("winCountWithStop",String.format("COUNTIF('MA%d汇总'!$N$2:$N$%d,\">0\")",i,tradeList.size()+1)); //有止损盈利次数
-                                 statMap.put("lossCountWithStop",String.format("COUNTIF('MA%d汇总'!$N$2:$N$%d,\"<0\")",i,tradeList.size()+1)); //有止损亏损次数
-                                 statMap.put("maxWithStop",String.format("MAX('MA%d汇总'!$N$2:$N$%d)",i,tradeList.size()+1)); //单次最大盈利
-                                 statMap.put("minWithStop",String.format("MIN('MA%d汇总'!$N$2:$N$%d)",i,tradeList.size()+1));//单次最大亏损
-                                 statMap.put("winCountWithoutStop",String.format("COUNTIF('MA%d汇总'!$M$2:$M$%d,\">0\")",i,tradeList.size()+1));//无止损盈利次数
-                                 statMap.put("lossCountWithoutStop",String.format("COUNTIF('MA%d汇总'!$M$2:$M$%d,\"<0\")",i,tradeList.size()+1));//无止损亏损次数
-                                 statMap.put("maxWithoutStop",String.format("MAX('MA%d汇总'!$M$2:$M$%d)",i,tradeList.size()+1));//单次最大盈利
-                                 statMap.put("minWithoutStop",String.format("MIN('MA%d汇总'!$M$2:$M$%d)",i,tradeList.size()+1));//单次最大亏损
-
-
-                                 statList.add(statMap);//将统计的Map加入到统计的列表
-                                 excelReader.modify(maFile, tradeList, i);
-
-
-                                 log.append("MA" + i + "处理完毕，亚克西\n");
+                                 log.append("文件写入完毕，文件路径为 ：" + maFile.getPath() + "\n");
                              }
 
-
-                             log.append("正在处理汇总表");
-
-                             excelReader.fullStatTable(maFile,statList,from);
-
-                             log.append("处理汇总表结束");
-
-                             log.append("文件写入完毕，文件路径为 ：" + maFile.getPath() + "\n");
 
 
                          } catch (Exception e1) {
